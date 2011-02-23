8dae1f8c49f6757da1bd49c747106c32e4a72824
marc.celani <marc.celani@4db5ec5a-08ab-0084-4c12-dd2ef2a14a4f>
2011-02-23 10:39:26 +0000

    Cleared Repository

--- Ticket To Ride/src/model/Card.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Card implements java.io.Serializable {
-	BLACK, WHITE, RED, YELLOW, ORANGE, GREEN, BLUE, PURPLE, ENGINE
-}
--- Ticket To Ride/src/model/GameFactory.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public interface GameFactory {
-
-	public Game create(ArrayList<Player> players);
-}
